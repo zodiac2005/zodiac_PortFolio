1491fe43367b8228b2f4d41d6d2f717807a27658
zodiac2005 <a.antra@edu.umi.ac.ma>
2026-05-15 06:03:41 +0200

    add some editing to portfolio.html

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
-var title = document.getElementById("t")
-title.innerHTML="zodiac"
+var title = document.getElementById("t");
+title.innerHTML = "zodiac";
+
+var slideImages = ["imgs/dashbored.png", "imgs/indeximg.png","imgs/Screenshot 2026-05-15 043452.png", "imgs/Screenshot 2026-05-15 051915.png"];
+var currentSlide = 0;
+var systemManagerImage = document.getElementById("system-manager-image");
+
+function rotateSystemManagerImage() {
+    if (!systemManagerImage) return;
+    currentSlide = (currentSlide + 1) % slideImages.length;
+    systemManagerImage.src = slideImages[currentSlide];
+}
+
+setInterval(rotateSystemManagerImage, 4000);
